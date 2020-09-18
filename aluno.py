6543df474dbf110cca61029d78ad79ccd784f420
anderson-mj <71097156+anderson-mj@users.noreply.github.com>
2020-09-17 21:59:14 -0300

    1o commit das classes Aluno, Curso e Disciplina
    classe Aluno recebe como parametros cada dado do aluno e armazena como atributo do objeto
    classe Curso recebe como parametros codigo, notas e cargas horarias referentes a cada curso e armazena como atributo do objeto
    classe Disciplina recebe a matricula do aluno, armazena como atributo uma lista de tuplas contendo a disciplina e o ano/semestre em que foi cursada pelo aluno

---
+++ aluno.py
@@ -0,0 +1,8 @@
+class Aluno:
+
+    def __init__(self, matricula, disciplinas_com_ano, cod_curso, nota, carga):
+        self.matricula = matricula
+        self.disciplinas_com_ano = disciplinas_com_ano
+        self.cod_curso = cod_curso
+        self.nota = nota
+        self.carga = carga
